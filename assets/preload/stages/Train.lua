93643ab08a13cfeb3a11c0940e3501d1a5bc1aba
XD Mods <120686314+XDMODS@users.noreply.github.com>
2023-01-02 05:25:50 -0300

    Add files via upload

---
+++ assets/preload/stages/Train.lua
@@ -0,0 +1,15 @@
+function onCreate()
+
+
+    -- background shit
+
+    makeLuaSprite('BG1', 'Backgrounds/BG1_train', -400, -200);
+
+    setLuaSpriteScrollFactor('BG1', 0.95, 0.95);
+
+    scaleObject('BG1', 1.2, 1.2);
+
+    addLuaSprite('BG1', false);
+    
+
+end
